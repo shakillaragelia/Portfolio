d1ece42773079ebd1fe2b5378f9dec4bec2c8ac4
Shakilla Ragelia <shakilla.ragelia25@gmail.com>
2026-04-02 17:54:36 +0700

    fix app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
-<html lang="en" class="scroll-smooth">
+<html lang="id" class="scroll-smooth">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Portfolio - Full Stack Developer</title>
-    
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
+
+    <title inertia>{{ config('app.name', 'Portfolio') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
-<body class="font-sans antialiased bg-slate-950 text-slate-200 selection:bg-indigo-500 selection:text-white">
+<body>
     @inertia
 </body>
 </html>
