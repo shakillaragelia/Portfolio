754b636b99a1ae985fe43a7284ae123eb2e30f1b
Shakilla Ragelia <shakilla.ragelia25@gmail.com>
2026-04-04 14:08:51 +0700

    update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,20 @@
-import './bootstrap';
-import '../css/app.css';
-
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import '../css/app.css'
 
 createInertiaApp({
-    title: (title) => title ? `${title} - Portfolio` : 'Portfolio',
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
-        return pages[`./Pages/${name}.vue`];
-    },
+    resolve: name =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue')
+        ),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
-            .mount(el);
+            .mount(el)
     },
-});
+    progress: {
+        color: '#00b4d8',
+    },
+})
