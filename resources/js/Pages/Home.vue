5e88c335fd8443c90ddb5c362ad762d3b48a903d
Shakilla Ragelia <shakilla.ragelia25@gmail.com>
2026-04-14 18:35:28 +0700

    modify home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,10 +1,9 @@
 <script setup>
-import NavBar          from '@/Components/sections/NavBar.vue'
-import HeroSection     from '@/Components/sections/HeroSection.vue'
-import AboutSection    from '@/Components/sections/AboutSection.vue'
-import SkillsSection   from '@/Components/sections/SkillsSection.vue'
-import ProjectsSection from '@/Components/sections/ProjectsSection.vue'
-import ContactSection  from '@/Components/sections/ContactSection.vue'
+import NavBar           from '@/Components/sections/NavBar.vue'
+import HeroSection      from '@/Components/sections/HeroSection.vue'
+import AboutSection     from '@/Components/sections/AboutSection.vue'
+import PortfolioSection from '@/Components/sections/PortfolioSection.vue'
+import ContactSection   from '@/Components/sections/ContactSection.vue'
 import { Head } from '@inertiajs/vue3'
 
 defineProps({
@@ -19,11 +18,10 @@ defineProps({
     <div class="min-h-screen">
         <NavBar :settings="settings" />
         <main>
-            <HeroSection     :settings="settings" />
-            <AboutSection    :settings="settings" />
-            <SkillsSection />
-            <ProjectsSection :projects="projects" />
-            <ContactSection  :settings="settings" :comments="comments" />
+            <HeroSection      :settings="settings" />
+            <AboutSection     :settings="settings" />
+            <PortfolioSection :projects="projects" />
+            <ContactSection   :settings="settings" :comments="comments" />
         </main>
         <footer class="border-t border-[rgba(0,180,216,0.1)] py-6 text-center">
             <p class="font-mono text-xs tracking-widest"
